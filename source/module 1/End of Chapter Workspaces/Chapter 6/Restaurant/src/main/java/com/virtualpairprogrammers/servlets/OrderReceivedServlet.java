562dd97fedce8215ff49baf0c8260fcfb6bf3736
Joao Berardo <jberardo@me.com>
2020-05-26 18:50:50 -0700

    adding initial project files

---
+++ source/module 1/End of Chapter Workspaces/Chapter 6/Restaurant/src/main/java/com/virtualpairprogrammers/servlets/OrderReceivedServlet.java
@@ -0,0 +1,41 @@
+package com.virtualpairprogrammers.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.virtualpairprogrammers.data.MenuDataService;
+import com.virtualpairprogrammers.domain.MenuItem;
+
+public class OrderReceivedServlet extends HttpServlet {
+	
+	MenuDataService menuDataService = new MenuDataService();
+	
+	public void doPost (HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		int maxId = menuDataService.getFullMenu().size();
+		for (int i = 0; i <maxId; i++) {
+			String quantity = request.getParameter("item_" + i);
+			 try  
+			  {  
+			    int q = Integer.parseInt(quantity);
+			    if (q > 0) menuDataService.addToOrder(menuDataService.getItem(i), q);
+			  }  
+			  catch(NumberFormatException nfe)  
+			  {  
+			    //that's fine it just means there wasn't an order for this item 
+			  }  
+			  
+		}
+		
+		System.out.println("A new order has been received.");
+		
+		Double total = menuDataService.getOrderTotal();
+	
+		response.sendRedirect("/thankYou.html?total=" + total);
+	}
+}
